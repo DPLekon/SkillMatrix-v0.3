f4d7001b55c28c60154db38621c723e129707217
DPLekon <dp@lekon.dk>
2023-06-08 13:52:40 +0200

    first commit

---
+++ src/Component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CompetenceMatrix from './CompetenceMatrix';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Application</h1>
+      <CompetenceMatrix />
+    </div>
+  );
+};
+
+export default App;
