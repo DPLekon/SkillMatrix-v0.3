662b58e2e14b0faf1196687d51aabab95e56c42e
DPLekon <dp@lekon.dk>
2023-06-20 09:37:20 +0200

    SkillMatrix v0.3

--- src/App.js
+++ src/App.js
@@ -2,35 +2,88 @@ import React, { useState } from 'react';
 import './SkillMatrix.css';
 
 const SkillMatrix = () => {
-  const [skills, setSkills] = useState([
-    { id: 1, name: 'Skill 1', rating: 3 },
-    { id: 2, name: 'Skill 2', rating: 2 },
-    { id: 3, name: 'Skill 3', rating: 4 },
-    // Add more skills here
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      name: 'User 1',
+      skills: [
+        { id: 1, name: 'Skill 1', rating: 1 },
+        { id: 2, name: 'Skill 2', rating: 2 },
+        { id: 3, name: 'Skill 3', rating: 3 },
+        { id: 4, name: 'Skill 4', rating: 4 },
+      ],
+    },
+    // Add more users here
   ]);
 
-  const handleSkillChange = (id, newSkill) => {
-    const updatedSkills = skills.map((skill) =>
-      skill.id === id ? { ...skill, name: newSkill } : skill
+  const handleUserNameChange = (userId, newName) => {
+    const updatedUsers = users.map((user) =>
+      user.id === userId ? { ...user, name: newName } : user
     );
-    setSkills(updatedSkills);
+    setUsers(updatedUsers);
   };
 
-  const handleRatingChange = (id, newRating) => {
-    const updatedSkills = skills.map((skill) =>
-      skill.id === id ? { ...skill, rating: newRating } : skill
-    );
-    setSkills(updatedSkills);
+  const handleSkillChange = (userId, skillId, newSkill) => {
+    const updatedUsers = users.map((user) => {
+      if (user.id === userId) {
+        const updatedSkills = user.skills.map((skill) =>
+          skill.id === skillId ? { ...skill, name: newSkill } : skill
+        );
+        return { ...user, skills: updatedSkills };
+      }
+      return user;
+    });
+    setUsers(updatedUsers);
   };
 
-  const handleAddSkill = () => {
+  const handleRatingChange = (userId, skillId, newRating) => {
+    const updatedUsers = users.map((user) => {
+      if (user.id === userId) {
+        const updatedSkills = user.skills.map((skill) =>
+          skill.id === skillId ? { ...skill, rating: newRating } : skill
+        );
+        return { ...user, skills: updatedSkills };
+      }
+      return user;
+    });
+    setUsers(updatedUsers);
+  };
+
+  const handleAddSkill = (userId) => {
     const newSkill = { id: Date.now(), name: 'New Skill', rating: 1 };
-    setSkills([...skills, newSkill]);
+    const updatedUsers = users.map((user) => {
+      if (user.id === userId) {
+        const updatedSkills = [...user.skills, newSkill];
+        return { ...user, skills: updatedSkills };
+      }
+      return user;
+    });
+    setUsers(updatedUsers);
   };
 
-  const handleRemoveSkill = (id) => {
-    const updatedSkills = skills.filter((skill) => skill.id !== id);
-    setSkills(updatedSkills);
+  const handleRemoveSkill = (userId, skillId) => {
+    const updatedUsers = users.map((user) => {
+      if (user.id === userId) {
+        const updatedSkills = user.skills.filter((skill) => skill.id !== skillId);
+        return { ...user, skills: updatedSkills };
+      }
+      return user;
+    });
+    setUsers(updatedUsers);
+  };
+
+  const handleAddUser = () => {
+    const newUser = {
+      id: Date.now(),
+      name: 'New User',
+      skills: [{ id: Date.now() + 1, name: 'New Skill', rating: 1 }],
+    };
+    setUsers([...users, newUser]);
+  };
+
+  const handleRemoveUser = (userId) => {
+    const updatedUsers = users.filter((user) => user.id !== userId);
+    setUsers(updatedUsers);
   };
 
   const renderCircleParts = (rating) => {
@@ -41,7 +94,7 @@ const SkillMatrix = () => {
       const partColor = colors[i];
       const partStyle = {
         backgroundColor: partColor,
-        transform: `rotate(${i * 270}deg)`
+        transform: `rotate(${i * 270}deg)`,
       };
       circleParts.push(<div key={i} className="circle-part" style={partStyle} />);
     }
@@ -51,40 +104,76 @@ const SkillMatrix = () => {
 
   return (
     <div className="skill-matrix">
-      {skills.map((skill) => (
-        <div key={skill.id} className="skill-circle">
-          <input
-            type="text"
-            value={skill.name}
-            onChange={(event) => handleSkillChange(skill.id, event.target.value)}
-          />
-          <div className="circle">{renderCircleParts(skill.rating)}</div>
-          <div className="rating-buttons">
+      {users.map((user) => (
+        <div key={user.id} className="user-skills">
+          <div className="user-name">
+            <input
+              type="text"
+              value={user.name}
+              onChange={(event) => handleUserNameChange(user.id, event.target.value)}
+            />
             <button
-              onClick={() =>
-                handleRatingChange(skill.id, Math.max(skill.rating - 1, 1))
-              }
+              className="remove-user"
+              onClick={() => handleRemoveUser(user.id)}
             >
-              -
-            </button>
-            <button
-              onClick={() =>
-                handleRatingChange(skill.id, Math.min(skill.rating + 1, 4))
-              }
-            >
-              +
+              Remove User
             </button>
           </div>
-          <button className="remove-skill" onClick={() => handleRemoveSkill(skill.id)}>
-            Remove
+          <div className="skill-circles">
+            {user.skills.map((skill) => (
+              <div key={skill.id} className="skill-circle">
+                <input
+                  type="text"
+                  value={skill.name}
+                  onChange={(event) =>
+                    handleSkillChange(user.id, skill.id, event.target.value)
+                  }
+                />
+                <div className="circle">{renderCircleParts(skill.rating)}</div>
+                <div className="rating-buttons">
+                  <button
+                    onClick={() =>
+                      handleRatingChange(
+                        user.id,
+                        skill.id,
+                        Math.max(skill.rating - 1, 1)
+                      )
+                    }
+                  >
+                    -
+                  </button>
+                  <button
+                    onClick={() =>
+                      handleRatingChange(
+                        user.id,
+                        skill.id,
+                        Math.min(skill.rating + 1, 4)
+                      )
+                    }
+                  >
+                    +
+                  </button>
+                </div>
+                <button
+                  className="remove-skill"
+                  onClick={() => handleRemoveSkill(user.id, skill.id)}
+                >
+                  Remove Skill
+                </button>
+              </div>
+            ))}
+          </div>
+          <button className="add-skill-button" onClick={() => handleAddSkill(user.id)}>
+            Add Skill
           </button>
         </div>
       ))}
-      <button className="add-skill-button" onClick={handleAddSkill}>
-        Add Skill
+      <button className="add-user-button" onClick={handleAddUser}>
+        Add User
       </button>
     </div>
   );
 };
 
 export default SkillMatrix;
+
